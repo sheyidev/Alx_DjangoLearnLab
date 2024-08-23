283de5e7d58eec4a5dd87fe7b3fb8aa628a25f96
Sheyi <seezie@gmail.com>
2024-08-22 20:57:56 -0500

    working on it

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -13,8 +13,11 @@ class Book(models.Model):
 # Create your models here.
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book, related_name='books')
+    books = models.ManyToManyField(Book, related_name='libraries')
 # Create your models here.
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='library')
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
+    def __str__(self):
+        return self.name
