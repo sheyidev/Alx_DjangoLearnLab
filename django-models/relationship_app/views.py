f35f47ec3d51067f24a37389aea8681b4f659fee
Sheyi <seezie@gmail.com>
2024-08-24 21:34:06 -0500

    working on it

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Book
+
 
 # Create your views here.
+def list_all_books(request):
+    """
+    A function-based view to list all books with their titles and authors.
+    """
+    books = Book.objects.all()  ## Retrieve all book objects from the database
+    context = {'books': books}  # # Pass the books to the template context
+    return render(request, 'relationship_app/book_list.html', context)
