4a45d5b95f960ab3dda38d0a549d45e1fad1f73d
Sheyi <seezie@gmail.com>
2024-08-25 15:52:55 -0500

    working on it

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import View
-
+from django.contrib.auth import login
 
 # Create your views here.
 def list_books(request):
