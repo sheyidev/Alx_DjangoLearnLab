1f123e1cdd2380a434a09902e3c5ffa40559374a
Sheyi <seezie@gmail.com>
2024-08-27 23:05:49 -0500

    working

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'.'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
