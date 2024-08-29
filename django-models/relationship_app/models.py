1d7f0c7f97d73edb9c8d3eba0302cec1d2fe445b
Sheyi <seezie@gmail.com>
2024-08-28 21:09:04 -0500

    working on ORM

---
+++ django-models/relationship_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+##author: This is another attribute (field) of the Book model that represents a relationship between the Book model and the Author model.
+## ForeignKey: This is a field type in Django that creates a many-to-one relationship with another model. In this context, it means that each Book is associated with one Author, but each Author can be associated with multiple books.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+class Library(models.Model):
+     name = models.CharField(max_length=100)
+     books = models.ManyToManyField(Book,related_name='libraries')
+class Liberian(models.Model):
+      name = models.CharField(max_length=100)
+      library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='liberian')
---
+++ django-models/relationship_app/query_samples.py
@@ -0,0 +1,37 @@
+"""
+Prepare a Python script query_samples.py in the relationship_app directory. This script should contain the query for each of the following of relationship:
+Query all books by a specific author.
+List all books in a library.
+Retrieve the librarian for a library.
+
+"""
+from .models import Book, Author, Library,Liberian
+##query all the books first from the Book class
+books = Book.objects.all()
+
+
+## filter by author becuase of the instance
+books_by_author = Book.objects.filter(author='author')
+
+## list all books in library
+
+## first get the library instance
+library = Library.objects.get(name='name')
+
+books_in_library = books.objects.all()
+
+## Liberian for a libary
+## get the librarry instance first
+library_instance = Library.objects.get(library='library_name')
+
+##use the lib instance to search for the liberian for the lib
+"""
+Accessing Librarian from Library:
+
+Since you want to find the librarian for a given library, you should start with the Library instance and use the related_name defined in the OneToOneField to access the associated Librarian.
+"""
+liberian = library_instance.liberian.get()
+
+"""
+The OneToOneField creates a reverse relationship which allows you to navigate from Librarian to Library. However, if your starting point is the Library, you should follow the relationship from Library to Librarian.
+"""
