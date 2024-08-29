9ad0d6bf419e976d8b5d00b8f4a8b75102f4b50c
Sheyi <seezie@gmail.com>
2024-08-28 21:34:09 -0500

    working on models

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -6,20 +6,20 @@ class Author(models.Model):
 ##author: This is another attribute (field) of the Book model that represents a relationship between the Book model and the Author model.
 ## ForeignKey: This is a field type in Django that creates a many-to-one relationship with another model. In this context, it means that each Book is associated with one Author, but each Author can be associated with multiple books.
     def __str__(self) -> str:
-         return super().__str__()
+         return self.name
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     def __str__(self) -> str:
-         return super().__str__()
+        return self.title
 class Library(models.Model):
      name = models.CharField(max_length=100)
      books = models.ManyToManyField(Book,related_name='libraries')
 
      def __str__(self) -> str:
-         return super().__str__()
+         return self.name
 class Liberian(models.Model):
       name = models.CharField(max_length=100)
       library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='liberian')
       def __str__(self) -> str:
-         return super().__str__()
+         return self.name
