3a3e5b347d6644b3efee0f7d6e22ac5b2e8d9326
John <j.gaji@meneses.law>
2024-08-30 11:57:08 -0500

    working on urls

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -15,5 +15,7 @@ urlpatterns = [
     #path('register/', views.register, name="templates/accounts/register"),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # Logout view
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # Login view
-
+    path('librarian/dashboard/', views.librarian_dashboard, name= 'librarian_dashboard'),
+    path('admin/dashboard/', views.admin_dashboard, name= 'admin_dashboard'),
+    path('member/dashboard/', views.member_dashboard, name= 'member_dashboard')
 ]
