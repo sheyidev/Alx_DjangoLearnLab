961785d9dcef278dc3576cc5c91f08934774b388
Sheyi <seezie@gmail.com>
2024-08-31 12:59:30 -0500

    working on local file

--- django-models/relationship_app/forms.py
+++ django-models/relationship_app/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-#class SignUpForm(UserCreationForm):
+# class SignUpForm(UserCreationForm):
   #  class Meta:
   #      model = User
    #     fields = ['username', 'password1', 'password2']
