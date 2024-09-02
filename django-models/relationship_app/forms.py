5da47b4cd627352728bce897df33170946c1bd61
Sheyi <seezie@gmail.com>
2024-09-02 17:37:24 -0500

    working

--- django-models/relationship_app/forms.py
+++ django-models/relationship_app/forms.py
@@ -3,14 +3,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+##from  .models import 
 
 # class SignUpForm(UserCreationForm):
   #  class Meta:
   #      model = User
    #     fields = ['username', 'password1', 'password2']
+"""
 class RegistrationForm(UserCreationForm):
      email = forms.EmailField(required=True)
 
      class Meta:
           model = User
           fields = ['username', 'email', 'password1', 'password2']
+"""
