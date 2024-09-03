6c5c4b7afbbab92e41b1ddcabfb6c789c052b28d
Sheyi <seezie@gmail.com>
2024-09-02 19:09:40 -0500

    working on it

--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 ## resources used
 ##https://www.geeksforgeeks.org/how-to-use-user-model-in-django/
 # Create your models here.
@@ -68,27 +67,3 @@ class UserProfile(models.Model):
   # @receiver(post_save, sender=User)
   # def save_profile(sender, instance, **kwargs):
   #     instance.profile.save()
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(email, password, **extra_fields)
-    
-class CustomerUser(AbstractUser):
-    objects = CustomUserManager()
