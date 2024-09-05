2534204bdbcfeb9dca6ddba8672c51cf9edd8dc9
Sheyi <seezie@gmail.com>
2024-09-04 20:37:26 -0500

    work

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -5,5 +5,5 @@ from .models import Book, CustomUser, CustomerUserAdmin
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
-    list_filter = ('publication_year',)
+    list_filter = ('publication_year')
 admin.site.register(CustomUser, CustomerUserAdmin)
