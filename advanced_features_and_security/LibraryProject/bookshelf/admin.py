64e0264a266cce663ddf14533abe1d0c05c33593
Sheyi <seezie@gmail.com>
2024-09-04 21:11:11 -0500

    working

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -9,3 +9,8 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
     list_filter = ('publication_year')
 admin.site.register(CustomUser, CustomerUserAdmin)
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'date_joined')
+    search_fields = ('username', 'email')
