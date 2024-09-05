304df18bb991929853ccb9da3b90d8e01a36e98c
Sheyi <seezie@gmail.com>
2024-09-04 20:46:07 -0500

    work

--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -115,6 +115,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'bookshelf.CustomerUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
