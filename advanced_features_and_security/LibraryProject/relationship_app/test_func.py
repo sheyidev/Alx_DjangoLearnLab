57f12c7e7d57e45b35e8d8d99b3325faefe4ebd2
John <j.gaji@meneses.law>
2024-09-05 11:13:46 -0500

    deleted the whole folder

--- advanced_features_and_security/LibraryProject/relationship_app/test_func.py
+++
@@ -1,8 +0,0 @@
-def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
-
-def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
