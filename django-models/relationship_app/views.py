66eeff5e0acc96fb99def7e792c4a99522aba53c
Sheyi <seezie@gmail.com>
2024-09-05 20:44:03 -0500

    working

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -25,7 +25,7 @@ def home(request):
 def list_books(request):
       books = Book.objects.all()
       
-      return render(request, 'list_books.html',{'books':books} )
+      return render(request, 'relationship_app/list_books.html',{'books':books} )
 
 ##Create a class-based view in relationship_app/views.py that displays details for a specific library, listing all books available in that library.
 
