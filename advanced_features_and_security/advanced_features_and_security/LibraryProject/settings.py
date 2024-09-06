28bfe2444a44f9879fbf2e5319c26b7fe38c7972
Sheyi <seezie@gmail.com>
2024-09-05 21:49:53 -0500

    working on it

--- advanced_features_and_security/advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/advanced_features_and_security/LibraryProject/settings.py
@@ -124,4 +124,5 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 ## resources used
 ##https://www.geeksforgeeks.org/how-to-use-user-model-in-django/
 # Create your models here.
@@ -59,7 +60,7 @@ class UserProfile(models.Model):
        return f"{self.user.username} - {self.role}"
    
    @receiver(post_save, sender=User)
-   def create_profile(sender, instance, created, **kwargs):
+   def create_user_profile(sender, instance, created, **kwargs):
        if created:
            UserProfile.objects.create(user=instance)
        instance.userprofile.save()
@@ -67,3 +68,35 @@ class UserProfile(models.Model):
   # @receiver(post_save, sender=User)
   # def save_profile(sender, instance, **kwargs):
   #     instance.profile.save()
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+# Link the manager to your CustomUser model
+class CustomUser(AbstractUser):
+    objects = CustomUserManager()
