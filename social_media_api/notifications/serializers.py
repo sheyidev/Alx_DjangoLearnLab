78c3ed86d96844aebc3d3186c525d6c024f6e27f
John <j.gaji@meneses.law>
2024-10-04 11:22:14 -0500

    deleted some new projects

--- social_media_api/notifications/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Notification
-
-class NotificationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notification
-        fields = ['id', 'actor', 'verb', 'timestamp', 'target']
