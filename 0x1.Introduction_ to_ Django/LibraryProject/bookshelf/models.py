abad6bbfe6fd34062be38046cab7e7b6693aec68
Sheyi <seezie@gmail.com>
2024-10-05 06:05:30 -0500

    done with project

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/models.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
-title = models.CharField(max_length=200)
-author = models.CharField(max_length=100)
-publication_year = models.IntegerField()
+class Book(models.Model):
+
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
